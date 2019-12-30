cbdbfc5a569d7f5e53b214bea73b7da1858a7129
Marzenah <37334652+Marzenah@users.noreply.github.com>
2019-12-29 20:58:05 -0500

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -10,7 +10,7 @@ from search_modules import*
 def main():
     '''This is the main driver to start the whole program of Propublica Sample Searches'''
 
-    window = Tk()   # this is the container for GUI
+    window = Tk()   # this is the container for the GUI
 
     # customizing the window container
     window.title('   Welcome to ProPublica Sample Searches')
@@ -43,32 +43,32 @@ class GuiApp():
         label_main = Label(self.window, pady = 5, text='Choose search type', font='Georgia 16 bold')
         label_main.grid(row=1, columnspan=3)
 
-        rd1 = Radiobutton(self.window,text='1: Get the list of current Congressional representatives for a given state', variable=self.choice_var, value=1)
-        rd1.grid(row=2, column=0, padx=15, pady=18, sticky=W)
+        rd1 = Radiobutton(self.window,text='1: Get the list of Congressional reps for a given state ->', variable=self.choice_var, value=1)
+        rd1.grid(row=2, column=0, padx=8, pady=18, sticky=W)
 
-        label1_search1 = Label(self.window, text="Enter a two-letter postal code for the state:   ")
+        label1_search1 = Label(self.window, text="Enter the state's two-letter code:   ")
         label1_search1.grid(row=2, column=1, pady=18, sticky=E)
 
         entry_state = Entry(self.window, textvariable=self.state_code, justify=LEFT, bg="light grey")
         entry_state.grid(row=2, column=2, pady=18, sticky=W)
 
-        rd2 = Radiobutton(self.window, text='2: Search current or past representative by name', variable=self.choice_var, value=2)
-        rd2.grid(row=3, column=0, padx=15, pady=5, sticky=W)
+        rd2 = Radiobutton(self.window, text='2: Search current or past representative by name ->', variable=self.choice_var, value=2)
+        rd2.grid(row=3, column=0, padx=8, pady=5, sticky=W)
 
-        label1_search2 = Label(self.window, text="Enter last name:   ")
+        label1_search2 = Label(self.window, text="Enter rep's last name:   ")
         label1_search2.grid(row=3, column=1, sticky=E)
 
         entry_last_name = Entry(self.window, textvariable=self.last_name, bg="light grey")
         entry_last_name.grid(row=3, column=2, sticky=W)
 
-        label2_search2 = Label(self.window, text="Enter first name:   ")
+        label2_search2 = Label(self.window, text="Enter rep's first name:   ")
         label2_search2.grid(row=4, column=1, sticky=E)
 
         entry_first_name = Entry(self.window, textvariable=self.first_name, bg="light grey")
         entry_first_name.grid(row=4, column=2, sticky=W)
 
-        rd3 = Radiobutton(self.window, text='3: Get historical data on average age in Congress', variable=self.choice_var, value=3)
-        rd3.grid(row=6, column=0, padx=15, sticky=W)
+        rd3 = Radiobutton(self.window, text='3: Get graph for historical data on average age in Congress', variable=self.choice_var, value=3)
+        rd3.grid(row=6, column=0, padx=8, sticky=W)
 
         btn_search = Button(self.window, text='Search', bg='red', fg='white', command=self.show_data)
         btn_search.grid(row=8, columnspan=3, pady=5)
